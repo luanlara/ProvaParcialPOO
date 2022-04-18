5c4b6d509edebcd3c54e0d666df3e3781428ffbb
Guilherme Joaquim <guilherme.joaquim@acad.espm.br>
2022-04-18 17:30:50 -0300

    Create Reserva.java
    Criei a classe Reserva

---
+++ Reserva.java
@@ -0,0 +1,17 @@
+public class Reserva {
+    
+    Cliente cliente = new Cliente();
+
+    boolean pagamentoAVista;
+
+    public void Reserva(Cliente cliente, boolean pagamentoAVista){
+        Cliente c = cliente;
+
+
+    }
+
+    @Override
+    public String toString() {
+        return " {Reserva: nome -> " + "}";
+    }
+}
